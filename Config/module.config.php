525c16850ee5086e52eebabaa75eb11bfe77020c
David Yang <daworld.ny@gmail.com>
2016-09-04 20:46:25 +0500

    Added initial configuration

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,4 +1,10 @@
 <?php
 
 return array(
+ 'name' => 'Booking',
+ 'caption' => 'Booking',
+ 'route' => 'Booking:Admin:Calendar@indexAction',
+ 'icon' => 'fa fa-calendar fa-5x',
+ 'order' => 10,
+ 'description' => 'Booking module lets your visitors schedule various actions'
 );
